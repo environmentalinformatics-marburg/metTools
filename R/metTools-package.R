6d8a4e4e7a3730a5ad8d8ac2dadca488805ae9a5
tnauss <nausst@gmail.com>
2017-02-13 10:02:15 +0100

    Initial commi

---
+++ R/metTools-package.R
@@ -0,0 +1,15 @@
+#' Tools for meteorological analysis
+#'
+#' We provide a set of functions usefull for some of our common meteorological
+#' analysis studies.
+#'
+#' @name metTools-package
+#' @aliases metToolsToolspackage
+#' @docType package
+#' @title Tools for meteorological analysis
+#' @author Thomas Nauss
+#'
+#' @keywords package
+#'
+NULL
+
---
+++ R/readMetData.R
@@ -0,0 +1,38 @@
+#' Read meteorological data
+#'
+#' @description
+#' Read meteorological data from files provided in the format of our
+#' climate database.
+#'
+#' @param filepath Full path and name of the csv file containing the data.
+#'
+#' @return
+#' A \code{data.frame} object.
+#'
+#' @author
+#' Thomas Nauss
+#'
+#' @references
+#' NONE
+#'
+#' @examples
+#' \notrun{
+#' }
+#'
+#' @export readMetData
+#' @name readMetData
+#'
+readMetData <- function(filepath, ...){
+  df_met <- read.table(filepath, header = TRUE, sep = ",", dec = ".")
+
+  # Define grouping by year
+  df_met$g_a <- substr(as.character(df_met$datetime), 1, 4)
+
+  # Define grouping by exploratory and land cover type
+  df_met$g_belc <- substr(as.character(df_met$plotID), 1, 3)
+
+  # Define grouping by plot and year
+  df_met$g_pa <- paste0(as.character(df_met$plotID), "_",
+                        substr(as.character(df_met$datetime), 1, 4))
+  return(df_met)
+}
