caed9c804b7d3ad732138540f48160a1dba93d16
tnauss <nauss@staff.uni-marburg.de>
2018-10-01 22:41:55 +0200

    Update

--- R/dwd_io_stations_hourly.R
+++ R/dwd_io_stations_hourly.R
@@ -3,6 +3,7 @@
 dwd_io_stations_hourly <- function(filepath, ...){
   dwd_stat <- read.table(filepath, header = TRUE, sep = ";", dec = ".")
   dwd_stat$MESS_DATUM = strptime(dwd_stat$MESS_DATUM, format = "%Y%m%d%H", tz = "UTC")
+  dwd_stat$MESS_DATUM = as.POSIXct(dwd_stat$MESS_DATUM)
   colnames(dwd_stat)[c(2,4,5)] = c("datetime", "Ta_200", "rH_200")
   return(dwd_stat)
 }
